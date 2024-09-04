dbad4cd24c438dc789a53652db5d616b9371f66b
henki wisnu subakti <henki.subakti@gmail.com>
2024-09-04 16:45:53 +0700

    logout

--- resources/views/components/app/header.blade.php
+++ resources/views/components/app/header.blade.php
@@ -305,9 +305,10 @@
                     </ul>
                     <ul class="py-1 text-gray-700 dark:text-gray-300" aria-labelledby="dropdown">
                         <li>
-                            <a href="#"
-                                class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign
-                                out</a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                            <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Sign out</a>
                         </li>
                     </ul>
                 </div>
